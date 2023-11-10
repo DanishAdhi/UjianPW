28882a52292f1dcd95607b6e9fc88de47811703a
Danishvara Ramardhi Adhi <144645047+DanishAdhi@users.noreply.github.com>
2023-11-11 00:41:25 +0700

    Add files via upload

--- web_statis/js/utsscript.js
+++ web_statis/js/utsscript.js
@@ -10,25 +10,27 @@ function closeNav() {
 }
 
 // digital clock
+
 // Calling showTime function at every second
 setInterval(showTime, 1000);
 
 // Defining showTime funcion
 function showTime() {
 	// Getting current time 
-   let time = new Date();
+    let time = new Date();
 	let hour = time.getHours();
 	let min = time.getMinutes();
 	let sec = time.getSeconds();
-	am_pm = "AM";
+	am_pm = "PM";
 
 	
 	if (hour >= 12) {
 		if (hour > 12) hour -= 12;
-		am_pm = "PM";
+		am_pm = "PM";;
 	} else if (hour == 0) {
 		hr = 12;
 		am_pm = "AM";
+		
 	}
 
 	hour =
